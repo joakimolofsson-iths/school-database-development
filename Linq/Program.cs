d6acf24a032121b79059388d028300e30f87b1f5
Joakim Olofsson <joakim.olofsson@iths.se>
2024-12-05 17:30:50 +0100

    Practice

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
+            var myQuery = from num in numbers
+                          where num > 5
+                          select num;
+
+            numbers.Add(20);
+
+            foreach(int num in myQuery)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
